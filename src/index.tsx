f4c7d1c124b73b93cec97217e219a04ad172c6f2
윤성용 <iop017779@gmail.com>
2022-06-28 22:37:55 +0900

    FEAT: 카카오 공유 추가

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,12 @@ import reportWebVitals from './reportWebVitals';
 import App from './App';
 import './index.css';
 
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
