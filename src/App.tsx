e3a05161dff5e9182d2335c3da95411cb3532765
seora <seorachoi08@gmail.com>
2022-07-07 18:20:26 +0900

    feat : mypage, msg UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Main from './pages/Main';
+import Message from './pages/Message';
+import MessageWrite from './pages/MessageWrite';
+import Mypage from './pages/Mypage';
 
 const App:React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Main} />
+
+
+
+
+
+      
+      <Route path="/mypage" exact component={Mypage} />
+      <Route path="/messageList" exact component={Message} />
+      <Route path="/messageWrite" exact component={MessageWrite} />
+
+      <Redirect path="*" to="/" />
     </Switch>
   </BrowserRouter>
 )
